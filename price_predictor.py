6844f18dd278e502142759172e34627f10637b7c
GOGOJO <joshualu76@gmail.com>
2025-08-03 16:49:40 -0700

    found optimal tree size; 500

--- price_predictor.py
+++ price_predictor.py
@@ -2,30 +2,31 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.metrics import mean_absolute_error
 import matplotlib.pyplot as plt
 
+# Load data
 cali_data = pd.read_csv("housing.csv")
 
-# print(cali_data.columns)
-
-# thing we want to predict 
+# Target variable
 y = cali_data.median_house_value
 
-#values we use to predict  y 
-cali_features = ['longitude', 'latitude', 'housing_median_age', 'total_rooms', 'total_bedrooms','ocean_proximity']
-X = cali_data[cali_features]
+# Features (including a categorical one)
+cali_features = ['longitude', 'latitude', 'housing_median_age', 'total_rooms', 'total_bedrooms', 'ocean_proximity']
+X = pd.get_dummies(cali_data[cali_features])  # One-hot encode 'ocean_proximity'
 
-# data split for training vs validation data
-train_X, val_X, train_y, val_y = train_test_split(X, y,random_state = 0)
+# Split into training and validation sets
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
 
+# Function to get MAE for a given tree size
 def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
     model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
     model.fit(train_X, train_y)
     preds_val = model.predict(val_X)
     mae = mean_absolute_error(val_y, preds_val)
-    return(mae)
+    return mae
 
+# Evaluate the model with various tree sizes
 for max_leaf_nodes in [5, 50, 500, 5000]:
     my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
-    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
+    print("Max leaf nodes: %d \t Mean Absolute Error: %d" % (max_leaf_nodes, my_mae))
